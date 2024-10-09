f3add8642284ed5e2456695f31044944a039638e
joshu <joshu@SCHOOL-PC>
2024-10-09 16:10:42 -0500

    Add project files.

---
+++ CSCI1101 FirstProject/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "You are " << age << " years old" << endl;
+
+
+    return 0;
+}
